cfafd35ce31fa5b1953931b9695c01088514ea24
ruben montani <ruben_montani@hotmail.com>
2024-10-02 14:15:24 -0300

    node server

---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const path = require("path")
+
+
+const startServer=(option)=>{
+    const {port, public_path=public}=option
+
+    const app=express()
+//usamos el middleware  y definimos que carpeta public este disponible
+    app.use(express.static(public_path))
+
+//cualquier consulta a la url mostrara la carpeta
+app.get("*", (req,res)=>{
+
+    const indexPath=path.join(__dirname + `../../../${public_path}/index.html`)
+    res.sendFile(indexPath)
+})
+
+//si visitimamos la url en el puerto especificado, veremos la pagina
+app.listen(port,()=>{console.log("app escuchando en el puerto", port)})
+
+}
+
+
+module.exports={
+    startServer
+}
